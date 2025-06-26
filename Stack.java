69d7df001bb3c6c2a532914f1384d745b7fa2c6e
Kiara Mutschler <43933594+kjmutsch@users.noreply.github.com>
2025-06-26 16:49:54 -0500

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,37 +1,54 @@
-// Stack class using generics
+// Stack class using generic array
 class MyStack<T> {
-    // Internal storage for stack elements
     private T[] elements;
     private int top;
     private int capacity;
 
     // Constructor
     public MyStack(int capacity) {
+        this.capacity = capacity;
+        this.elements = (T[]) new Object[capacity];
     }
 
     public void push(T element) {
-      // TODO
+        if (size == capacity) resize();
+        elements[++top] = element;
     }
 
     public T pop() {
-      // TODO
+        if(isEmpty()) return;
+        T elementTemp = elements[top]
+        elements[top--] = null
+        return elementTemp;
     }
 
     public T peek() {
-      // TODO
+        if(isEmpty()) return;
+        return elements[top];
     }
 
     public boolean isEmpty() {
-      // TODO
+      return top == -1;
     }
 
     public int size() {
-      // TODO
+      return top + 1;
+    }
+
+    public void print() {
+        if(isEmpty()) return;
+        for(int i = top; i >= 0; i--) {
+            if(i == 0) System.out.println(i);
+            else {
+                System.out.println(i + ' => ');
+            }
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         MyStack<Integer> stack = new MyStack<>(10);
+        //stack.push
     }
 }
